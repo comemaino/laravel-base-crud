3f6cb17848664639427eac5f2306ca5af531acc9
comemaino <mat.maino@gmail.com>
2022-06-30 17:03:32 +0200

    Add: style

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,17 +2,21 @@
 
 @section('main_content')
 	<h1>COMICS</h1>
-	<ul>
-		@foreach ($comics_list as $comic)
-			<li>
-				<img src="{{ $comic->thumb }}" alt="">
-				<h4>
-					<a href="{{ route('comics.show', ['comic' => $comic->id]) }}">
-						{{ $comic->title }}
-					</a>
-				</h4>
-				<p>$ {{ $comic->price }}</p>
-			</li>
-		@endforeach
-	</ul>
+	<div class="container">
+		<ul class="d-flex flex-wrap p-0">
+			@foreach ($comics_list as $comic)
+				<li class="p-2 m-2">
+					<div class="d-flex flex-column h-100 _card">
+						<img src="{{ $comic->thumb }}" alt="">
+						<h4 class="mt-2">
+							<a href="{{ route('comics.show', ['comic' => $comic->id]) }}">
+								{{ $comic->title }}
+							</a>
+						</h4>
+						<p class="price text-center">$ {{ $comic->price }}</p>
+					</div>
+				</li>
+			@endforeach
+		</ul>
+	</div>
 @endsection
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.app')
 
 @section('main_content')
-	<div>
-		<img src="{{ $comics->thumb }}" alt="{{ $comics->title }}">
-		<h3>{{ $comics->title }}</h3>
-		<p>$ {{ $comics->price }}</p>
-		<p>{{ $comics->description }}</p>
-		<p>Series: {{ $comics->series }}</p>
-		<p>Date of release: {{ $comics->sale_date }}</p>
-		<p>Type: {{ $comics->type }}</p>
+	<div class="show container mt-5">
+		<div>
+			<img src="{{ $comics->thumb }}" alt="{{ $comics->title }}">
+			<h3>{{ $comics->title }}</h3>
+			<p>$ {{ $comics->price }}</p>
+			<p>{{ $comics->description }}</p>
+			<p>Series: {{ $comics->series }}</p>
+			<p>Date of release: {{ $comics->sale_date }}</p>
+			<p>Type: {{ $comics->type }}</p>
+		</div>
 	</div>
 @endsection
